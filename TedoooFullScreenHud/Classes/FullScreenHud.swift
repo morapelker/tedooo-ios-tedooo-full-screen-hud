d03b6f4d6d2b29b31ecec2e0cfc03cb422ac1bb3
morapelker <morapelker@gmail.com>
2022-07-07 15:16:40 +0300

    "v1.0.2"

--- TedoooFullScreenHud/Classes/FullScreenHud.swift
+++ TedoooFullScreenHud/Classes/FullScreenHud.swift
@@ -8,12 +8,20 @@
 import Foundation
 import JGProgressHUD
 
-class FullScreenHud: UIView {
+public class FullScreenHud: UIView {
     
-    let hud = JGProgressHUD()
-    var dismissAction: (() -> ())? = nil
+    public init() {
+        super.init(frame: .zero)
+    }
     
-    var indicatorView: JGProgressHUDIndicatorView? {
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public let hud = JGProgressHUD()
+    public var dismissAction: (() -> ())? = nil
+    
+    public var indicatorView: JGProgressHUDIndicatorView? {
         get {
             self.hud.indicatorView
         }
@@ -21,14 +29,14 @@ class FullScreenHud: UIView {
             self.hud.indicatorView = newValue
         }
     }
-    lazy var textLabel = self.hud.textLabel
+    public lazy var textLabel = self.hud.textLabel
     
-    func setBackground(shown: Bool) {
+    public func setBackground(shown: Bool) {
         self.backgroundColor = shown ? .init(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.5) : .clear
         self.isUserInteractionEnabled = shown
     }
     
-    func dismiss() {
+    public func dismiss() {
         UIView.animate(withDuration: 0.3, animations: {
             self.alpha = 0.0
         }) { (_) in
@@ -37,14 +45,14 @@ class FullScreenHud: UIView {
         }
     }
     
-    func dismiss(afterDelay: Double, onEnd: (() -> ())? = nil) {
+    public func dismiss(afterDelay: Double, onEnd: (() -> ())? = nil) {
         DispatchQueue.main.asyncAfter(deadline: .now() + afterDelay) { [weak self] in
             self?.dismiss()
             onEnd?()
         }
     }
     
-    var spin: Bool {
+    public var spin: Bool {
         get { isHidden }
         set {
             if newValue {
@@ -55,7 +63,7 @@ class FullScreenHud: UIView {
         }
     }
     
-    func show() {
+    public func show() {
         guard let window = UIApplication.shared.keyWindow else { return }
         self.removeFromSuperview()
         window.addSubview(self)
